d6836d2edb005651bd9677cc8d345c8bd36d1dc1
nishizuka <nishizuka23@gmail.com>
2016-12-01 01:15:41 -0200

    First implementation

---
+++ app/components/Main.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Main = React.createClass({
+  render: function() {
+    return <div>
+    </div>;
+  },
+});
+
+ReactDOM.render(<Main/>, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: "./app/components/Main.js",
+  output: {
+    path: "./public",
+    filename: "bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devServer: {
+    inline: true,
+    contentBase: './public',
+    port: 8000,
+  },
+}
