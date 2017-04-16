bcd18391f9c09c678c149e04d1beeb25fddefb96
toketas <nishizuka23@gmail.com>
2017-04-16 03:28:23 -0300

    Add css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,47 @@
-module.exports = {
-  entry: "./app/index.jsx",
+var path = require('path'),
+    webpack = require('webpack'),
+    extractTextPlugin = require('extract-text-webpack-plugin');
+
+var cssExtractTextPlugin = new extractTextPlugin('dist/styles.css');
+
+const config = {
+  entry: {
+    "script": "./scripts/index.jsx",
+    "style": "./styles/index.less",
+  },
   output: {
-    path: "./public",
-    filename: "bundle.js",
+    path: __dirname,
+    filename: "dist/[name].js",
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
+        exclude: /node_modules/,
+        loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
-        }
-      }
+        },
+        include: path.join(__dirname, 'scripts')
+      },
+      {
+        test: /\.css?$/,
+        loader: cssExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+      },
+      {
+        test: /\.less?$/,
+        loader: cssExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!less-loader' })
+      },
     ]
   },
+  plugins: [
+    cssExtractTextPlugin,
+  ],
   devServer: {
     inline: true,
-    contentBase: './',
+    contentBase: './dist',
     port: 8000,
   },
 }
+
+module.exports = config;
